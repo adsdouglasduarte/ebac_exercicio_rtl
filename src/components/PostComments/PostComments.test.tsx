02e45f038975dd7ac3ab8c9e9a79c125c77ed826
Douglas Duarte <adsdouglasduarte@gmail.com>
2026-02-13 22:15:50 -0300

    FIX: ajuste de URL desatualizada

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,34 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from '@testing-library/react'
+import PostComment from '.'
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+
+  it('Deve renderizar o componente corretamente', () => {
+    render(<PostComment />)
+    expect(screen.getByText('Comentar')).toBeInTheDocument()
+  })
+
+  it('Deve adicionar dois comentários', () => {
+    render(<PostComment />)
+
+    const comentarioInput = screen.getByTestId('comentario-input')
+    const botaoSubmit = screen.getByTestId('submit-btn')
+
+    // Primeiro comentário
+    fireEvent.change(comentarioInput, {
+      target: { value: 'Primeiro comentário' }
+    })
+    fireEvent.click(botaoSubmit)
+
+    // Segundo comentário
+    fireEvent.change(comentarioInput, {
+      target: { value: 'Segundo comentário' }
+    })
+    fireEvent.click(botaoSubmit)
+
+    // Verifica se os dois aparecem na tela
+    expect(screen.getByText('Primeiro comentário')).toBeInTheDocument()
+    expect(screen.getByText('Segundo comentário')).toBeInTheDocument()
+  })
+
+})
--- src/components/PostComments/index.tsx
+++ src/components/PostComments/index.tsx
@@ -16,7 +16,7 @@ const Post = () => {
 
     return (
         <div>
-            <ul className={styles['post-comments']}>
+            <ul data-testid="lista-comentarios" className={styles['post-comments']}>
                 {comments.map(({ comment, id }) => (
                     <li className={styles['post-comment']} key={id}>
                         <p className={styles['post-comment-content']}>
@@ -26,8 +26,8 @@ const Post = () => {
                 ))}
             </ul>
             <form onSubmit={handleAddComment} className={styles['post-comments-form']}>
-                <textarea value={tempComment} onChange={e => setTempComment(e.target.value)} required className={styles['post-comments-form-textarea']} />
-                <button type="submit" className={styles['post-comments-form-button']}>
+                <textarea value={tempComment} onChange={e => setTempComment(e.target.value)} required className={styles['post-comments-form-textarea']} data-testid="comentario-input" />
+               <button type="submit" className={styles['post-comments-form-button']} data-testid="submit-btn">
                     Comentar
                 </button>
             </form>
